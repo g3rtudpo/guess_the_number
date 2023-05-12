8d6462c19ff696b405247c689dab0901980ba00d
3nchant3d <133363632+3nchant3d@users.noreply.github.com>
2023-05-12 04:54:10 -0700

    Create main.py
    add code

---
+++ main.py
@@ -0,0 +1,25 @@
+import random
+
+number = random.randint(1, 10)
+guess = None
+guess_count = 0
+guess_limit = 3
+out_of_guesses = False
+
+print("Я загадал число от 1 до 10. У тебя есть три попытки.")
+
+while guess != number and not out_of_guesses:
+    if guess_count < guess_limit:
+        guess = int(input("Введи число: "))
+        guess_count += 1
+        if guess < number:
+            print("Мое число больше твоего.")
+        elif guess > number:
+            print("Мое число меньше твоего.")
+    else:
+        out_of_guesses = True
+
+if out_of_guesses:
+    print("Ты проиграл. Я загадал число", number)
+else:
+    print("Ты выиграл! Я загадал число", number, "и тебе удалось угадать его за", guess_count, "попыток.")
